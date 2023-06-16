76a1bf715440b70374e39ef1b221266b81a356a0
Rezlang <96161550+Rezlang@users.noreply.github.com>
2023-06-16 14:21:36 -0400

    updates to homepage

--- homepage/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from homepage import views
-
-urlpatterns = [
-    path("", views.home, name="index"),
-    # path("hello/contacts", views.contacts, name="contacts"),
-]
